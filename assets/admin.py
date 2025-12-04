1384915f5c7a6cde5a9ca0fa6c9f73cd3b6771c0
Jay Merlan <jr.merlan@gmail.com>
2025-12-03 21:56:37 -0800

    Registered models for admin

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Asset
+
 # Register your models here.
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    pass
--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Project, Snapshot
 
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unique_id')
+    search_fields = ('name', 'unique_id')
+
+@admin.register(Snapshot)
+class SnapshotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project', 'date')
+    list_filter = ('project', 'date')
+    search_fields = ('name', 'project__name')
